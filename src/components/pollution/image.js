28f55f90f0ea453769a6d0124d82e95615cc9e68
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2017-07-25 17:56:12 +0200

    Add the different background images

--- src/components/pollution/image.js
+++ src/components/pollution/image.js
@@ -5,8 +5,8 @@ class Image extends Component {
 	render () {
 		return (
 		  <div className="air-img" >
-				<img className="air-img-common air-img-good" src="images/good.svg" alt="good pollution"></img>
-				<img className="air-img-common-mobile air-img-good-mobile" src="images/good-mobile.svg" alt="good pollution"></img>
+				<img className="air-img-common air-img-good" src="images/backgrounds/good.svg" alt="good pollution"></img>
+				<img className="air-img-common-mobile air-img-good-mobile" src="images/backgrounds/good-mobile.svg" alt="good pollution"></img>
 			</div>
 		);
 	}
